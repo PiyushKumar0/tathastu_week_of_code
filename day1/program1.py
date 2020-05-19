1c85c170fc5cfc85ee6cc9049db59930e6f1712e
PiyushKumar0 <65599939+PiyushKumar0@users.noreply.github.com>
2020-05-19 16:45:54 +0530

    Create program1.py

---
+++ day1/program1.py
@@ -0,0 +1,13 @@
+#https://www.linkedin.com/in/piyushkumar0
+
+name = input("Enter your name:")
+branch = input("\nEnter your Branch:")
+gender = input("\nEnter your Gender:")
+college = input("\nEnter your College:")
+age = input("\nEnter your Age:")
+
+print("\n\nYour name is:",name)
+print("Your branch is:",branch)
+print("Your gender is:",gender)
+print("Your college is:",college)
+print("Your age is:",age)
