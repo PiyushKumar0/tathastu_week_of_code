5dec78c36801ec4df36fe942ee54cdef9c0afd35
Piyush Kumar <65599939+PiyushKumar0@users.noreply.github.com>
2020-05-24 17:58:00 +0530

    Create program3.py

---
+++ day6/program3.py
@@ -0,0 +1,13 @@
+#https://www.linkedin.com/in/piyushkumar0/
+
+size = int(input("Enter size of array: "))
+li = []
+for i in range(size):
+    li.append(int(input("Enter element number " + str(i + 1) + " : ")))
+
+pos = 1
+for i in li:
+    if i==pos:
+        pos+=1
+        
+print("Smallest positive number not in the list is:", pos)
