7df7cfd57859bf5199ab3c4f1c6d7bc9670091a1
Piyush Kumar <65599939+PiyushKumar0@users.noreply.github.com>
2020-05-24 18:37:03 +0530

    Update program4.py

--- day6/program4.py
+++ day6/program4.py
@@ -15,14 +15,14 @@ def cyclic(N):
     
     rem = num % 10; 
     div = math.floor(num / 10); 
-    num = ((math.pow(10, n - 1)) * rem + div);
+    num = ((math.pow(10, n - 1)) * rem + div)
     large = num
     
     while (1): 
 
         rem = num % 10; 
         div = math.floor(num / 10); 
-        num = ((math.pow(10, n - 1)) * rem + div);
+        num = ((math.pow(10, n - 1)) * rem + div)
         
         if(num > N and num < large):
             large = num
@@ -36,4 +36,4 @@ def cyclic(N):
         print("The next larger number than",N,"is",large)
         
 num = int(input("Enter a number: "))
-cyclic(num); 
+cyclic(num)
